a53d744f8dc449059080727759a049416f3b0145
eternalconcert <ckokoska@gmx.de>
2021-10-08 21:53:31 +0200

    added toasts

---
+++ src/Toast.tsx
@@ -0,0 +1,50 @@
+import React, { FC, ReactNode } from 'react';
+import { BehaviorSubject } from 'rxjs';
+import { useObservable } from './Hooks';
+
+interface Toast {
+  title: string,
+  body: string,
+  variant?: string,
+  timeout?: number,
+}
+
+const toastStream = new BehaviorSubject<Toast[]>([]);
+
+export function pushToast(toast: Toast): void {
+  toastStream.next([...toastStream.getValue(), toast]);
+  setTimeout(() => {
+    toastStream.next(toastStream.getValue().filter((t) => t !== toast));
+  }, toast.timeout || 10000);
+}
+
+export const ToastList: FC = () => {
+  const messages = useObservable(toastStream) || [];
+
+  function handleClose(message: Toast) {
+    toastStream.next(toastStream.getValue().filter((t) => t !== message));
+  }
+
+  return (
+    <div className={'toast'}>
+      { messages.map((toast, index) => (
+        <div key={index} className={`card bg-${toast.variant || 'success'} mb-3`} style={{ width: '20rem' }}>
+          <div className={'card-header text-white'}>
+            {toast.title}
+            <button
+              style={{ right: '5px', top: '2px', position: 'absolute' }}
+              type={'button'}
+              className={'btn'}
+              onClick={() => { handleClose(toast); }}
+            >
+              {'x'}
+            </button>
+          </div>
+          <div className={'card-body bg-white'}>
+            <p className={'card-text'}>{toast.body}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
